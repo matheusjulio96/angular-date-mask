d662ff246b672706e28a8a0adc88eaae9b071482
matheusjulio96 <matheus.julio96@hotmail.com>
2019-12-14 19:28:50 -0300

    add mask using inputmask on primeng calendar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
+import * as Inputmask from "inputmask";
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent implements  AfterViewInit  {
+  name = 'Angular Date Mask';
+
+  value: Date;
+  @ViewChild('myCalendar', {static: true}) calendar;
+
+  ngAfterViewInit(){
+    var input = this.calendar.inputfieldViewChild.nativeElement;
+
+    Inputmask.default({ 
+      alias: "datetime", 
+      inputFormat: "dd/mm/yyyy", 
+      jitMasking: true}).mask(input);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import { CalendarModule } from 'primeng/calendar';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, 
+    CalendarModule, 
+    BrowserAnimationsModule
+ ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
